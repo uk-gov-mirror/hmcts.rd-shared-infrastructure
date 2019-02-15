ebc312a5fa6a78f1d7b9cc7ce6c63b13e08983fd
Ravi Aghera <Ravi.Aghera@HMCTS.net:wq>
2019-02-15 09:59:44 +0000

    Initial infrastructure without project group id

---
+++ key-vault.tf
@@ -0,0 +1,20 @@
+locals {
+  key_vault_name = "${var.product}-${var.env}"
+}
+
+module "ia_key_vault" {
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name                    = "${local.key_vault_name}"
+  location                = "${var.location}"
+  resource_group_name     = "${azurerm_resource_group.rg.name}"
+  tenant_id               = "${var.tenant_id}"
+  object_id               = "${var.jenkins_AAD_objectId}"
+  product_group_object_id = "${var.rd_product_group_object_id}"
+  env                     = "${var.env}"
+  product                 = "${var.product}"
+  common_tags             = "${local.common_tags}"
+}
+
+output "vaultName" {
+  value = "${local.key_vault_name}"
+}
---
+++ variables.tf
@@ -0,0 +1,62 @@
+variable "product" {
+  type        = "string"
+  default     = "rd"
+  description = "The name of your application"
+}
+
+variable "env" {
+  type        = "string"
+  description = "The deployment environment (sandbox, aat, prod etc..)"
+}
+
+variable "location" {
+  type    = "string"
+  default = "UK South"
+}
+
+variable "tenant_id" {
+  type        = "string"
+  description = "The Tenant ID of the Azure Active Directory"
+}
+
+variable "jenkins_AAD_objectId" {
+  type        = "string"
+  description = "This is the ID of the Application you wish to give access to the Key Vault via the access policy"
+}
+
+//TODO populate when this is available.
+variable "rd_product_group_object_id" {
+  type    = "string"
+  default = ""
+}
+
+// as of now, UK South is unavailable for Application Insights
+variable "appinsights_location" {
+  type        = "string"
+  default     = "West Europe"
+  description = "Location for Application Insights"
+}
+
+variable "appinsights_application_type" {
+  type        = "string"
+  default     = "Web"
+  description = "Type of Application Insights (Web/Other)"
+}
+
+variable "team_name" {
+  type        = "string"
+  description = "Team name"
+  default     = "Immigration"
+}
+
+variable "team_contact" {
+  type        = "string"
+  description = "Team contact"
+  default     = "#immigrationandasylum"
+}
+
+variable "destroy_me" {
+  type        = "string"
+  description = "Here be dragons! In the future if this is set to Yes then automation will delete this resource on a schedule. Please set to No unless you know what you are doing"
+  default     = "No"
+}
