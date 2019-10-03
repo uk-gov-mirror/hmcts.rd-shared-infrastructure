33a6a1ee4e981398ca6c501c0915c99f098c9a17
Tim Jacomb <tim.jacomb@hmcts.net>
2019-10-03 14:37:23 +0100

    Fix s2s secret (#3)

--- key-vault.tf
+++ key-vault.tf
@@ -18,6 +18,33 @@ module "rd_key_vault" {
   managed_identity_object_id = "${var.managed_identity_object_id}"
 }
 
+data "azurerm_key_vault" "s2s" {
+  name                = "s2s-${var.env}"
+  resource_group_name = "rpe-service-auth-provider-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "prd-s2s" {
+  name         = "microservicekey-rd-professional-api"
+  key_vault_id = "${data.azurerm_key_vault.s2s.id}"
+}
+
+resource "azurerm_key_vault_secret" "prd-s2s" {
+  name         = "s2s-secret"
+  value        = "${data.azurerm_key_vault_secret.prd-s2s.value}"
+  key_vault_id = "${module.rd_key_vault.key_vault_id}"
+}
+
+data "azurerm_key_vault_secret" "user-profile-s2s" {
+  name         = "microservicekey-rd-user-profile-api"
+  key_vault_id = "${data.azurerm_key_vault.s2s.id}"
+}
+
+resource "azurerm_key_vault_secret" "user-profile-s2s" {
+  name         = "up-s2s-secret"
+  value        = "${data.azurerm_key_vault_secret.user-profile-s2s.value}"
+  key_vault_id = "${module.rd_key_vault.key_vault_id}"
+}
+
 output "vaultName" {
   value = "${local.key_vault_name}"
 }
