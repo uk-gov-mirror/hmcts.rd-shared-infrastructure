fd10a8c332450df42de0846093d934a75330310a
Shashank Rastogi <shashank.rastogi@hmcts.net>
2020-01-20 10:19:10 +0000

    changed the location to UK West

--- storage-account.tf
+++ storage-account.tf
@@ -24,7 +24,7 @@ resource "azurerm_storage_account" "storage_account" {
   name                = "${local.account_name}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
 
-  location                 = "${azurerm_resource_group.rg.location}"
+  location                 = "UK West"
   account_tier             = "Standard"
   account_replication_type = "LRS"
   account_kind             = "BlobStorage"
