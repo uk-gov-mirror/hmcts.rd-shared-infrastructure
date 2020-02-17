14f5f7f124ceefdf87b2bbf10a37f45a48632b0c
Shashank Rastogi <shashank.rastogi@hmcts.net>
2020-02-17 00:09:58 +0000

    changed the  storage account configuration

--- storage-account.tf
+++ storage-account.tf
@@ -12,7 +12,7 @@ locals {
 }
 
 module "storage_account" {
-  source                    = "git@github.com:hmcts/cnp-module-storage-account.git?ref=0.0.1"
+  source                    = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
   env                       = "${var.env}"
   storage_account_name      = "${local.account_name}"
   resource_group_name       = "${azurerm_resource_group.rg.name}"
