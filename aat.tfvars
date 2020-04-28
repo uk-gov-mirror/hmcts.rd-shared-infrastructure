2b6a5a66a163b365157d679485d32bc4955557f9
vijayrajagopalan-hmcts <794168@cognizant.com>
2020-04-28 11:14:01 +0100

    Added environment variables

--- aat.tfvars
+++ aat.tfvars
@@ -1,3 +1,3 @@
 //external_hostname = "jrd.demo.platform.hmcts.net"
-aks_infra_subscription_id = "d025fece-ce99-4df2-b7a9-b649d3ff2060"
+aks_infra_subscription_id = "96c274ce-846d-4e48-89a7-d528432298a7"
 mgmt_subscription_id = "8999dec3-0104-4a27-94ee-6588559729d1"
--- storage-account.tf
+++ storage-account.tf
@@ -8,95 +8,95 @@ provider "azurerm" {
   subscription_id = "${var.mgmt_subscription_id}"
 }
 
-# provider "azurerm" {
-#   alias           = "mgmt"
-#   subscription_id = "${var.mgmt_subscription_id}"
+provider "azurerm" {
+  alias           = "mgmt"
+  subscription_id = "${var.mgmt_subscription_id}"
+}
+
+locals {
+  account_name          = "${replace("${var.product}${var.env}", "-", "")}"
+  mgmt_network_name     = "core-infra-vnet-demo"
+  mgmt_network_rg_name  = "core-infra-demo"
+
+  // for each client service two containers are created: one named after the service
+  // and another one, named {service_name}-rejected, for storing envelopes rejected by bulk-scan
+  client_service_names  = ["jud-ref-data"]
+}
+
+module "storage_account" {
+  source                   = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
+  env                      = "${var.env}"
+  storage_account_name     = "${local.account_name}"
+  resource_group_name      = "${azurerm_resource_group.rg.name}"
+  location                 = "${var.location}"
+  account_kind             = "BlobStorage"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  access_tier              = "Hot"
+
+  //  enable_blob_encryption    = true
+  //  enable_file_encryption    = true
+  enable_https_traffic_only = true
+
+  // Tags
+  common_tags  = "${local.tags}"
+  team_contact = "${var.team_contact}"
+  destroy_me   = "${var.destroy_me}"
+}
+
+# data "azurerm_virtual_network" "mgmt_vnet" {
+#   provider            = "azurerm.mgmt"
+#   name                = "${local.mgmt_network_name}"
+#   resource_group_name = "${local.mgmt_network_rg_name}"
 # }
 
-# locals {
-#   account_name          = "${replace("${var.product}${var.env}", "-", "")}"
-#   mgmt_network_name     = "core-infra-vnet-demo"
-#   mgmt_network_rg_name  = "core-infra-demo"
-
-#   // for each client service two containers are created: one named after the service
-#   // and another one, named {service_name}-rejected, for storing envelopes rejected by bulk-scan
-#   client_service_names  = ["jud-ref-data"]
+# data "azurerm_subnet" "jenkins_subnet" {
+#   provider             = "azurerm.mgmt"
+#   name                 = "iaas"
+#   virtual_network_name = "core-cftptl-intsvc-vnet"
+#   resource_group_name  = "aks-infra-cftptl-intsvc-rg"
 # }
 
-# module "storage_account" {
-#   source                   = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
-#   env                      = "${var.env}"
-#   storage_account_name     = "${local.account_name}"
-#   resource_group_name      = "${azurerm_resource_group.rg.name}"
-#   location                 = "${var.location}"
-#   account_kind             = "BlobStorage"
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-#   access_tier              = "Hot"
+resource "azurerm_storage_container" "service_containers" {
+  name                 = "${local.client_service_names[count.index]}"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  storage_account_name = "${module.storage_account.storageaccount_name}"
+  count                = "${length(local.client_service_names)}"
+}
 
-#   //  enable_blob_encryption    = true
-#   //  enable_file_encryption    = true
-#   enable_https_traffic_only = true
+resource "azurerm_storage_container" "service_rejected_containers" {
+  name                 = "${local.client_service_names[count.index]}-archive"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  storage_account_name = "${module.storage_account.storageaccount_name}"
+  count                = "${length(local.client_service_names)}"
+}
 
-#   // Tags
-#   common_tags  = "${local.tags}"
-#   team_contact = "${var.team_contact}"
-#   destroy_me   = "${var.destroy_me}"
-# }
+resource "azurerm_key_vault_secret" "storage_account_name" {
+  name          = "storage-account-name"
+  value         = "${module.storage_account.storageaccount_name}"
+  # vault_uri = "${data.azurerm_key_vault.key_vault.vault_uri}"
+  key_vault_id  = "${module.rd_key_vault.key_vault_id}"
+}
 
-# # data "azurerm_virtual_network" "mgmt_vnet" {
-# #   provider            = "azurerm.mgmt"
-# #   name                = "${local.mgmt_network_name}"
-# #   resource_group_name = "${local.mgmt_network_rg_name}"
-# # }
+resource "azurerm_key_vault_secret" "storageaccount_id" {
+  name         = "storage-account-id"
+  value        = "${module.storage_account.storageaccount_id}"
+  # key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+  key_vault_id  = "${module.rd_key_vault.key_vault_id}"
+}
 
-# # data "azurerm_subnet" "jenkins_subnet" {
-# #   provider             = "azurerm.mgmt"
-# #   name                 = "iaas"
-# #   virtual_network_name = "core-cftptl-intsvc-vnet"
-# #   resource_group_name  = "aks-infra-cftptl-intsvc-rg"
-# # }
+resource "azurerm_key_vault_secret" "storage_account_primary_key" {
+  name          = "storage-account-primary-key"
+  value         = "${module.storage_account.storageaccount_primary_access_key}"
+  # vault_uri = "${data.azurerm_key_vault.key_vault.vault_uri}"
+  key_vault_id  = "${module.rd_key_vault.key_vault_id}"
+}
 
-# resource "azurerm_storage_container" "service_containers" {
-#   name                 = "${local.client_service_names[count.index]}"
-#   resource_group_name  = "${azurerm_resource_group.rg.name}"
-#   storage_account_name = "${module.storage_account.storageaccount_name}"
-#   count                = "${length(local.client_service_names)}"
-# }
+output "storage_account_name" {
+  value = "${module.storage_account.storageaccount_name}"
+}
 
-# resource "azurerm_storage_container" "service_rejected_containers" {
-#   name                 = "${local.client_service_names[count.index]}-archive"
-#   resource_group_name  = "${azurerm_resource_group.rg.name}"
-#   storage_account_name = "${module.storage_account.storageaccount_name}"
-#   count                = "${length(local.client_service_names)}"
-# }
-
-# resource "azurerm_key_vault_secret" "storage_account_name" {
-#   name          = "storage-account-name"
-#   value         = "${module.storage_account.storageaccount_name}"
-#   # vault_uri = "${data.azurerm_key_vault.key_vault.vault_uri}"
-#   key_vault_id  = "${module.rd_key_vault.key_vault_id}"
-# }
-
-# resource "azurerm_key_vault_secret" "storageaccount_id" {
-#   name         = "storage-account-id"
-#   value        = "${module.storage_account.storageaccount_id}"
-#   # key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
-#   key_vault_id  = "${module.rd_key_vault.key_vault_id}"
-# }
-
-# resource "azurerm_key_vault_secret" "storage_account_primary_key" {
-#   name          = "storage-account-primary-key"
-#   value         = "${module.storage_account.storageaccount_primary_access_key}"
-#   # vault_uri = "${data.azurerm_key_vault.key_vault.vault_uri}"
-#   key_vault_id  = "${module.rd_key_vault.key_vault_id}"
-# }
-
-# output "storage_account_name" {
-#   value = "${module.storage_account.storageaccount_name}"
-# }
-
-# output "storage_account_primary_key" {
-#   sensitive = true
-#   value     = "${module.storage_account.storageaccount_primary_access_key}"
-# }
+output "storage_account_primary_key" {
+  sensitive = true
+  value     = "${module.storage_account.storageaccount_primary_access_key}"
+}
