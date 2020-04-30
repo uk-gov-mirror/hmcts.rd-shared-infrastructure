6f44b7aff55857911424e21c7edcea4d7b78bc31
abhijit <Abhijit.diwan@hmcts.net>
2020-04-30 15:06:39 +0100

    test: terraform value changes

--- key-vault.tf
+++ key-vault.tf
@@ -13,6 +13,9 @@ module "rd_key_vault" {
   env                     = "${var.env}"
   product                 = "${var.product}"
   common_tags             = "${local.common_tags}"
+
+  #aks migration
+  managed_identity_object_id = "${var.managed_identity_object_id}"
 }
 
 data "azurerm_key_vault" "key_vault" {
