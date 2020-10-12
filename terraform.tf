051f4ddbfa4e01baf5be4a2ea0d6e353c290b451
Bogdan Stanciu <bogdan.stanciu@hmcts.net>
2020-10-12 17:35:33 +0100

    add kv exception to handle soft-delete

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,11 @@
 provider "azurerm" {
   version = "=2.30.0"
-  features {}
+  features {
+    key_vault {
+      recover_soft_deleted_key_vaults = true
+      purge_soft_delete_on_destroy    = true
+    }
+  }
 }
 
 provider "azurerm" {
