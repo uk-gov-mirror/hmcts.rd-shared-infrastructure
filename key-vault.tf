681094f33f38bfd0d1cd0b9726a15df576263d0a
Vijay Kumar Rajagopalan <62386423+vijayrajagopalan-hmcts@users.noreply.github.com>
2020-11-02 17:14:49 +0000

    Delete rd-perftest Key Vault (#87)

--- key-vault.tf
+++ key-vault.tf
@@ -1,23 +1,23 @@
-locals {
-  key_vault_name = join("-", [var.product, var.env])
-}
+# locals {
+#   key_vault_name = join("-", [var.product, var.env])
+# }
 
-module "rd_key_vault" {
-  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=azurermv2"
-  name                    = local.key_vault_name
-  location                = var.location
-  resource_group_name     = azurerm_resource_group.rg.name
-  tenant_id               = var.tenant_id
-  object_id               = var.jenkins_AAD_objectId
-  product_group_object_id = var.rd_product_group_object_id
-  env                     = var.env
-  product                 = var.product
-  common_tags             = local.common_tags
+# module "rd_key_vault" {
+#   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=azurermv2"
+#   name                    = local.key_vault_name
+#   location                = var.location
+#   resource_group_name     = azurerm_resource_group.rg.name
+#   tenant_id               = var.tenant_id
+#   object_id               = var.jenkins_AAD_objectId
+#   product_group_object_id = var.rd_product_group_object_id
+#   env                     = var.env
+#   product                 = var.product
+#   common_tags             = local.common_tags
 
-  #aks migration
-  managed_identity_object_id = var.managed_identity_object_id
-}
+#   #aks migration
+#   managed_identity_object_id = var.managed_identity_object_id
+# }
 
-output "vaultName" {
-  value = module.rd_key_vault.key_vault_name
-}
+# output "vaultName" {
+#   value = module.rd_key_vault.key_vault_name
+# }
