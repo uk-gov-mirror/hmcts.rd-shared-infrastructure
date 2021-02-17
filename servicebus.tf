60cd60f426f51437387bf0bd65acd073b4933668
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2021-02-17 16:24:53 +0000

    Delete SB and related resources

--- servicebus.tf
+++ servicebus.tf
@@ -7,64 +7,64 @@ locals {
   resource_group_name       = "${azurerm_resource_group.rg.name}"
 }
 
-module "servicebus-namespace" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=servicebus_tf"
-  name                = "${local.servicebus_namespace_name}"
-  location            = "${var.location}"
-  env                 = "${var.env}"
-  common_tags         = "${local.common_tags}"
-  resource_group_name = "${local.resource_group_name}"
-}
+# module "servicebus-namespace" {
+#   source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
+#   name                = "${local.servicebus_namespace_name}"
+#   location            = "${var.location}"
+#   env                 = "${var.env}"
+#   common_tags         = "${local.common_tags}"
+#   resource_group_name = "${local.resource_group_name}"
+# }
 
-module "caseworker-topic" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-topic?ref=servicebus_topic_tf"
-  name                = "${local.caseworker_topic_name}"
-  namespace_name      = "${module.servicebus-namespace.name}"
-  resource_group_name = "${local.resource_group_name}"
-}
+# module "caseworker-topic" {
+#   source              = "git@github.com:hmcts/terraform-module-servicebus-topic?ref=master"
+#   name                = "${local.caseworker_topic_name}"
+#   namespace_name      = "${module.servicebus-namespace.name}"
+#   resource_group_name = "${local.resource_group_name}"
+# }
 
-module "caseworker-subscription" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=servicebus_subscription_tf"
-  name                = "${local.subscription_name}"
-  namespace_name      = "${module.servicebus-namespace.name}"
-  topic_name          = "${module.caseworker-topic.name}"
-  resource_group_name = "${local.resource_group_name}"
-}
+# module "caseworker-subscription" {
+#   source              = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=master"
+#   name                = "${local.subscription_name}"
+#   namespace_name      = "${module.servicebus-namespace.name}"
+#   topic_name          = "${module.caseworker-topic.name}"
+#   resource_group_name = "${local.resource_group_name}"
+# }
 
-module "caseworker-queue-1" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=master"
-  name                = local.queue_name1
-  namespace_name      = module.servicebus-namespace.name
-  resource_group_name = azurerm_resource_group.rg.name
-}
+# module "caseworker-queue-1" {
+#   source              = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=master"
+#   name                = local.queue_name1
+#   namespace_name      = module.servicebus-namespace.name
+#   resource_group_name = azurerm_resource_group.rg.name
+# }
 
-module "caseworker-queue-2" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=master"
-  name                = local.queue_name2
-  namespace_name      = module.servicebus-namespace.name
-  resource_group_name = azurerm_resource_group.rg.name
-}
+# module "caseworker-queue-2" {
+#   source              = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=master"
+#   name                = local.queue_name2
+#   namespace_name      = module.servicebus-namespace.name
+#   resource_group_name = azurerm_resource_group.rg.name
+# }
 
-resource "azurerm_key_vault_secret" "caseworker-topic-primary-send-listen-conn-str" {
-  name         = "caseworker-topic-primary-send-listen-connection-string"
-  value        = "${module.caseworker-topic.primary_send_and_listen_connection_string}"
-  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
-}
+# resource "azurerm_key_vault_secret" "caseworker-topic-primary-send-listen-conn-str" {
+#   name         = "caseworker-topic-primary-send-listen-connection-string"
+#   value        = "${module.caseworker-topic.primary_send_and_listen_connection_string}"
+#   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+# }
 
-resource "azurerm_key_vault_secret" "caseworker-topic-secondary-send-listen-conn-str" {
-  name         = "caseworker-topic-secondary-send-listen-connection-string"
-  value        = "${module.caseworker-topic.secondary_send_and_listen_connection_string}"
-  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
-}
+# resource "azurerm_key_vault_secret" "caseworker-topic-secondary-send-listen-conn-str" {
+#   name         = "caseworker-topic-secondary-send-listen-connection-string"
+#   value        = "${module.caseworker-topic.secondary_send_and_listen_connection_string}"
+#   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+# }
 
-resource "azurerm_key_vault_secret" "caseworker-topic-primary-send-listen-shared-access-key" {
-  name         = "caseworker-topic-primary-send-listen-shared-access-key"
-  value        = "${module.caseworker-topic.primary_send_and_listen_shared_access_key}"
-  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
-}
+# resource "azurerm_key_vault_secret" "caseworker-topic-primary-send-listen-shared-access-key" {
+#   name         = "caseworker-topic-primary-send-listen-shared-access-key"
+#   value        = "${module.caseworker-topic.primary_send_and_listen_shared_access_key}"
+#   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+# }
 
-resource "azurerm_key_vault_secret" "caseworker-topic-secondary-send-listen-shared-access-key" {
-  name         = "caseworker-topic-secondary-send-listen-shared-access-key"
-  value        = "${module.caseworker-topic.secondary_send_and_listen_shared_access_key}"
-  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
-}
+# resource "azurerm_key_vault_secret" "caseworker-topic-secondary-send-listen-shared-access-key" {
+#   name         = "caseworker-topic-secondary-send-listen-shared-access-key"
+#   value        = "${module.caseworker-topic.secondary_send_and_listen_shared_access_key}"
+#   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+# }
