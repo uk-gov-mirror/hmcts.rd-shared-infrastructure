2d851ae9be714a0d438a87c21e5236c48c0115fa
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2021-02-26 11:35:51 +0000

    Rename app insights secret name

--- application-insights.tf
+++ application-insights.tf
@@ -19,7 +19,7 @@ resource "azurerm_application_insights" "appinsights" {
 }
 
 resource "azurerm_key_vault_secret" "applicationinsights-instrumentation-key" {
-  name         = "AppInsightsInstrumentationKey"
+  name         = "ApplicationInsightsInstrumentationKey"
   value        = azurerm_application_insights.appinsights.instrumentation_key
   key_vault_id = module.rd_key_vault.key_vault_id
 }
