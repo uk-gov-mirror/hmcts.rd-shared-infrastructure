4ab0af2a8a8f8b7883f28c5b84e0b20663a11a49
Vijay Kumar Rajagopalan <62386423+vijayrajagopalan-hmcts@users.noreply.github.com>
2021-03-08 10:43:56 +0000

    Add topic for judicial and delete unwanted secrets (#112)

--- service-bus.tf
+++ service-bus.tf
@@ -2,6 +2,9 @@ locals {
   servicebus_namespace_name     = join("-", [var.product, "servicebus", var.env])
   caseworker_topic_name         = join("-", [var.product, "caseworker-topic", var.env])
   caseworker_subscription_name  = join("-", [var.product, "caseworker-subscription", var.env])
+  
+  judicial_topic_name           = join("-", [var.product, "judicial-topic", var.env])
+  judicial_subscription_name    = join("-", [var.product, "judicial-subscription", var.env])
   resource_group_name           = azurerm_resource_group.rg.name
 }
 
@@ -38,16 +41,28 @@ module "am-orm-test-pr-subscription" {
   resource_group_name   = local.resource_group_name
 }
 
-resource "azurerm_key_vault_secret" "caseworker-topic-primary-send-listen-conn-str" {
-  name         = "caseworker-topic-primary-send-listen-connection-string"
-  value        = module.caseworker-topic.primary_send_and_listen_connection_string
-  key_vault_id = module.rd_key_vault.key_vault_id
+module "judicial-topic" {
+  source                = "git@github.com:hmcts/terraform-module-servicebus-topic?ref=master"
+  name                  = local.judicial_topic_name
+  namespace_name        = module.servicebus-namespace.name
+  resource_group_name   = local.resource_group_name
 }
 
-resource "azurerm_key_vault_secret" "caseworker-topic-secondary-send-listen-conn-str" {
-  name         = "caseworker-topic-secondary-send-listen-connection-string"
-  value        = module.caseworker-topic.secondary_send_and_listen_connection_string
-  key_vault_id = module.rd_key_vault.key_vault_id
+module "judicial-subscription" {
+  source                = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=master"
+  name                  = local.judicial_subscription_name
+  namespace_name        = module.servicebus-namespace.name
+  topic_name            = module.judicial-topic.name
+  resource_group_name   = local.resource_group_name
+}
+
+module "am-orm-judicial-test-pr-subscription" {
+  source                = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=master"
+  count                 = lower(var.env) == "aat" ? 1 : 0
+  name                  = "am-orm-judicial-preview-functional-test"
+  namespace_name        = module.servicebus-namespace.name
+  topic_name            = module.judicial-topic.name
+  resource_group_name   = local.resource_group_name
 }
 
 resource "azurerm_key_vault_secret" "caseworker-topic-primary-send-listen-shared-access-key" {
@@ -56,8 +71,8 @@ resource "azurerm_key_vault_secret" "caseworker-topic-primary-send-listen-shared
   key_vault_id = module.rd_key_vault.key_vault_id
 }
 
-resource "azurerm_key_vault_secret" "caseworker-topic-secondary-send-listen-shared-access-key" {
-  name         = "caseworker-topic-secondary-send-listen-shared-access-key"
-  value        = module.caseworker-topic.secondary_send_and_listen_shared_access_key
+resource "azurerm_key_vault_secret" "judicial-topic-primary-send-listen-shared-access-key" {
+  name         = "judicial-topic-primary-send-listen-shared-access-key"
+  value        = module.judicial-topic.primary_send_and_listen_shared_access_key
   key_vault_id = module.rd_key_vault.key_vault_id
 }
