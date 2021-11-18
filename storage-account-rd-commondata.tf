262d6605642092e20deb3b1b3f02026d40a140b4
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2021-11-18 18:45:11 +0000

    Update resource names

--- storage-account-rd-commondata.tf
+++ storage-account-rd-commondata.tf
@@ -1,14 +1,14 @@
 locals {
   product                   = "rdcommondata"
-  rd_location_account_name  = join("", [local.product, var.env])
+  rd_cd_account_name        = join("", [local.product, var.env])
   # container_name            = "lrd-ref-data"
   # container_archive_name    = "lrd-ref-data-archive"
 }
 
-module "storage_account_rd_location" {
+module "storage_account_rd_commondata" {
   source                   = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
   env                      = var.env
-  storage_account_name     = local.rd_location_account_name
+  storage_account_name     = local.rd_cd_account_name
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_kind             = "StorageV2"
@@ -36,20 +36,20 @@ module "storage_account_rd_location" {
 #   storage_account_name = module.storage_account_rd_location.storageaccount_name
 # }
 
-resource "azurerm_key_vault_secret" "rd_location_storage_account_name" {
-  name          = "rd-location-storage-account-name"
-  value         = module.storage_account_rd_location.storageaccount_name
+resource "azurerm_key_vault_secret" "rd_cd_storage_account_name" {
+  name          = "rd-commondata-storage-account-name"
+  value         = module.storage_account_rd_commondata.storageaccount_name
   key_vault_id  = module.rd_key_vault.key_vault_id
 }
 
-resource "azurerm_key_vault_secret" "rd_location_storageaccount_id" {
-  name          = "rd-location-storage-account-id"
-  value         = module.storage_account_rd_location.storageaccount_id
+resource "azurerm_key_vault_secret" "rd_cd_storageaccount_id" {
+  name          = "rd-commondata-storage-account-id"
+  value         = module.storage_account_rd_commondata.storageaccount_id
   key_vault_id  = module.rd_key_vault.key_vault_id
 }
 
-resource "azurerm_key_vault_secret" "rd_location_storage_account_primary_key" {
-  name          = "rd-location-storage-account-primary-key"
-  value         = module.storage_account_rd_location.storageaccount_primary_access_key
+resource "azurerm_key_vault_secret" "rd_cd_storage_account_primary_key" {
+  name          = "rd-commondata-storage-account-primary-key"
+  value         = module.storage_account_rd_commondata.storageaccount_primary_access_key
   key_vault_id  = module.rd_key_vault.key_vault_id
 }
