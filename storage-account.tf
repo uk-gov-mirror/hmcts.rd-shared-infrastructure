353384ec5f3acbe1530f6a316d8148328267259b
mokainos <72137340+mokainos@users.noreply.github.com>
2021-12-14 15:51:18 +0000

    reverting

--- storage-account.tf
+++ storage-account.tf
@@ -9,7 +9,6 @@ module "storage_account" {
   storage_account_name     = local.account_name
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
-  account_kind             = "StorageV2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
   access_tier              = "Hot"
