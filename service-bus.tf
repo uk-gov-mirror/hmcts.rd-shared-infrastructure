f460a2ee29c10603c7240ba28b35ac168537a526
msl8r <72858053+msl8r@users.noreply.github.com>
2022-04-06 14:04:16 +0100

    DTSPO-6371 - Fix alias name

--- service-bus.tf
+++ service-bus.tf
@@ -11,7 +11,7 @@ locals {
 
 module "servicebus-namespace" {
   providers = {
-    azurerm.private-endpoint = azurerm.private-endpoint
+    azurerm.private_endpoint = azurerm.private_endpoint
   }
   
   source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
