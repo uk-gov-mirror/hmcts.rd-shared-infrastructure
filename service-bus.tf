3ed762d967d42716767610e0176520fe66c3a615
hmcts <prudhvi.maddineni@hmcts.net>
2023-01-13 13:20:02 +0000

    service bus configuration for Judicial

--- service-bus.tf
+++ service-bus.tf
@@ -3,7 +3,6 @@ locals {
   caseworker_topic_name                         = join("-", [var.product, "caseworker-topic", var.env])
   caseworker_subscription_name                  = join("-", [var.product, "caseworker-subscription", var.env])
 
-  judicial_servicebus_namespace_name            = join("-", [var.product, "servicebus", var.env])
   judicial_topic_name                           = join("-", [var.product, "judicial-topic", var.env])
   judicial_subscription_name                    = join("-", [var.product, "judicial-subscription", var.env])
 
