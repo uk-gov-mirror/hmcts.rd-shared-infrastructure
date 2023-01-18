a115caf292378622792957f0886f6c6654dc8ee3
jordankainos <j.brown@kainos.com>
2023-01-18 10:57:05 +0000

    Updating tags to comply with Azure policy

--- main.tf
+++ main.tf
@@ -5,6 +5,9 @@ locals {
     "Team Contact" = var.team_contact
     "Destroy Me"   = var.destroy_me
     "managedBy"    = var.team_name
+    "application"  = "referencedata"
+    "builtFrom"    = "https://github.com/hmcts/rd-shared-infrastructure"
+    "businessArea" = "CFT"
   }
 
   tags = merge(var.common_tags, {"Team Contact" = "#referencedata"})
