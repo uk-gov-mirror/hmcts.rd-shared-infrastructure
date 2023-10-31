77469e2c0b66f006e8a4406f135e91b6a9dff62a
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2023-10-31 11:39:39 +0000

    DTSRD-1466.fixing environment tag for rd-perftest

--- key-vault.tf
+++ key-vault.tf
@@ -12,7 +12,7 @@ module "rd_key_vault" {
   product_group_object_id = var.rd_product_group_object_id
   env                     = var.env
   product                 = var.product
-  common_tags             = local.common_tags
+  common_tags             = local.tags
   create_managed_identity = true
   additional_managed_identities_access = var.additional_managed_identities_access
 }
