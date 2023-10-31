7c172981ab6b7d127e319411472b01d68266a7a9
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-10-31 17:44:45 +0000

    Use environment from common_tags var

--- main.tf
+++ main.tf
@@ -1,16 +1,5 @@
 locals {
-  tags_kv = merge(var.common_tags, tomap({
-    "Team Name"    = var.team_name
-    "Team Contact" = var.team_contact
-    "Destroy Me"   = var.destroy_me
-    "managedBy"    = var.team_name
-    "application"  = "referencedata"
-    "builtFrom"    = "https://github.com/hmcts/rd-shared-infrastructure"
-    "businessArea" = "CFT"
-  }))
-
   tags = merge(var.common_tags, tomap({
-    "environment"  = var.env
     "Team Name"    = var.team_name
     "Team Contact" = var.team_contact
     "Destroy Me"   = var.destroy_me
