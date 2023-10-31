7da8bd69ffbda17cbaacb6fd58ebbd86da72db21
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-10-31 12:26:42 +0000

    Fix location field issue

--- application-insights.tf
+++ application-insights.tf
@@ -4,7 +4,6 @@ locals {
 
 resource "azurerm_application_insights" "appinsights" {
   name                = local.appinsights_name
-  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = var.appinsights_application_type
   tags                = local.tags
