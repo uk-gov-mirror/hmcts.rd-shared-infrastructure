f327cdb0c16b725bedad88e8e5594142fd36db16
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2023-10-31 11:34:59 +0000

    DTSRD-1466.fixing environment tag for rd-perftest

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
 locals {
-  common_tags = {
-    "Team Name"    = var.team_name
-    "Team Contact" = var.team_contact
-    "Destroy Me"   = var.destroy_me
-    "managedBy"    = var.team_name
-    "application"  = "referencedata"
-    "builtFrom"    = "https://github.com/hmcts/rd-shared-infrastructure"
-    "businessArea" = "CFT"
-  }
-
-  tags = merge(var.common_tags, {"Team Contact" = "#referencedata"})
+  tags = merge(
+    var.common_tags,
+    tomap({
+      "Team Contact" = "#referencedata"
+      "Destroy Me"   = var.destroy_me
+      "Team Name"    = var.team_name
+      "managedBy"    = var.team_name
+      "application"  = "referencedata"
+      "builtFrom"    = "https://github.com/hmcts/rd-shared-infrastructure"
+      "businessArea" = "CFT"
+    })
+    )
 }
