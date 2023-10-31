7036b90e160d292bab3ef8dd3fb9677cb64eb017
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-10-31 13:18:13 +0000

    Fix location field issue

--- resource-group.tf
+++ resource-group.tf
@@ -1,6 +1,8 @@
-
 resource "azurerm_resource_group" "rg" {
   name     = join("-", [var.product, var.env])
   location = var.location
-  tags     = merge(local.tags, {"lastUpdated" = timestamp()})
+  tags     = merge(local.tags, tomap({
+    "lastUpdated" = timestamp()
+    "environment" = var.env
+  }))
 }
