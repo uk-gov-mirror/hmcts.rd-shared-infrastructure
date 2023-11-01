74589941f0557cf852256ce0f6f68ad30a5ebee3
Andy Boyle <Andrew.Boyle@HMCTS.NET>
2023-11-01 13:52:39 +0000

    DTSRD-1205
    - New storage account and container for data extracts.

---
+++ storage-account-rd-data-extracts.tf
@@ -0,0 +1,47 @@
+locals {
+  product                       = "rddataextract"
+  rd_data_extract_account_name  = join("", [local.product, var.env])
+  container_name                = "rd-data-extract"
+}
+
+module "storage_account_rd_data_extract" {
+  source                   = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
+  env                      = var.env
+  storage_account_name     = local.rd_data_extract_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location
+  account_kind             = var.rd_data_extract_storage_account_kind
+  account_tier             = "Standard"
+  account_replication_type = var.rd_data_extract_storage_repl_type
+  access_tier              = var.rd_data_extract_storage_access_tier
+
+  enable_https_traffic_only = true
+
+  // Tags
+  common_tags  = local.tags
+  team_contact = var.team_contact
+  destroy_me   = var.destroy_me
+}
+
+resource "azurerm_storage_container" "data_extract_service_container" {
+  name                 = local.container_name
+  storage_account_name = module.storage_account_rd_data_extract.storage_account_name
+}
+
+resource "azurerm_key_vault_secret" "rd_data_extract_storage_account_name" {
+  name         = "rd-data-extract-storage-account-name"
+  value        = module.storage_account_rd_data_extract.storageaccount_name
+  key_vault_id = module.rd_key_vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "rd_data_extract_storageaccount_id" {
+  name         = "rd-data-extract-storage-account-id"
+  value        = module.storage_account_rd_commondata.storageaccount_id
+  key_vault_id = module.rd_key_vault.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "rd_data_extract_storage_account_primary_key" {
+  name         = "rd-data-extract-storage-account-primary-key"
+  value        = module.storage_account_rd_data_extract.storageaccount_primary_access_key
+  key_vault_id = module.rd_key_vault.key_vault_id
+}
