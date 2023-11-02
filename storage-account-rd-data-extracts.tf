d9493dc9d052adfd002bc5a82c5f649d32184030
Andy Boyle <Andrew.Boyle@HMCTS.NET>
2023-11-02 10:45:24 +0000

    DTSRD-1205
    - Adding subnets to see if this allows the jenkins build agent access to create a container.

--- storage-account-rd-data-extracts.tf
+++ storage-account-rd-data-extracts.tf
@@ -21,6 +21,8 @@ module "storage_account_rd_data_extract" {
   common_tags  = local.tags
   team_contact = var.team_contact
   destroy_me   = var.destroy_me
+
+  sa_subnets = local.all_valid_subnets
 }
 
 resource "azurerm_storage_container" "data_extract_service_container" {
