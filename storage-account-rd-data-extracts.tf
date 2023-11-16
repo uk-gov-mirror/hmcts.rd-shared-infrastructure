8e5de80cb3d2ee6e8aa178b4633b11798821eca8
andy-m-b <141227049+andy-m-b@users.noreply.github.com>
2023-11-16 14:29:57 +0000

    DTSRD-1205
    - Updating to include the PIM functionality that has been introduced to cnp-module-storage-account.

--- storage-account-rd-data-extracts.tf
+++ storage-account-rd-data-extracts.tf
@@ -39,7 +39,14 @@ module "storage_account_rd_data_extract" {
 
   enable_https_traffic_only = true
 
-  pim_roles = var.env != prod ? {} : local.pim_roles
+  pim_roles = var.env != prod ? {} : {
+    "Storage Account Delegator" = {
+      principal_id = data.azuread_group.sc_group.id
+    }
+    "Storage Blob Delegator" = {
+      principal_id = data.azuread_group.sc_group.id
+    }
+  }
 
   // Tags
   common_tags  = local.tags
