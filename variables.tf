fdfd345f3e2205aba4dba7dfafe3c9188b9e193c
andy-m-b <141227049+andy-m-b@users.noreply.github.com>
2023-11-16 17:20:45 +0000

    DTSRD-1205
    - Updating to include the PIM functionality that has been introduced to cnp-module-storage-account.

--- variables.tf
+++ variables.tf
@@ -175,18 +175,3 @@ variable "role_assignments" {
     "Storage Blob Data Reader"
   ]
 }
-
-variable "pim_roles" {
-  type = map(object({
-    principal_id = string
-  }))
-  default     = {
-    "Storage Account Delegator" = {
-      principal_id = data.azuread_group.sc_group.id
-    }
-    "Storage Blob Delegator" = {
-      principal_id = data.azuread_group.sc_group.id
-    }
-  }
-  description = "{ 'Role name' = { principal_id = 'principal_id' } }, only certain roles are supported"
-}
